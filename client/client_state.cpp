804c1367360690f0a06b5af690ade2167b10173c
ArpineAramyan <api2002@mail.ru>
2024-06-04 14:29:22 +0400

    removed extra spaces

--- client/client_state.cpp
+++ client/client_state.cpp
@@ -281,11 +281,11 @@ void CLIENT_STATE::show_host_info() {
 #endif
     }
     if (host_info.docker_use){
-￼        msg_printf(NULL, MSG_INFO, "Docker is available");
-￼    }
-￼    else{
-￼        msg_printf(NULL, MSG_INFO, "Docker is not available for running task");
-￼    }
+        msg_printf(NULL, MSG_INFO, "Docker is available");
+    }
+    else{
+        msg_printf(NULL, MSG_INFO, "Docker is not available for running task");
+    }
 }
 
 int rsc_index(const char* name) {
